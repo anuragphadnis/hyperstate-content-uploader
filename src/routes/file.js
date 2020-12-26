3d946aa8770bf75e134245d635e9fd5636349446
Anurag Phadnis <phadnis.anurag@gmail.com>
2020-12-26 07:36:34 +0530

    Added endpoint to get uploaded file

--- src/routes/file.js
+++ src/routes/file.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import path from 'path';
 import FileController from '../controllers/file.controller';
 import convertImageJob from '../services/convert.image.job.service';
 import dbService from '../services/db.service';
+import config from '../config/config';
 
 const router = express.Router();
 
@@ -29,6 +31,22 @@ router.post('/', FileController.upload.any('files'), async (req, res) => {
     console.log(`Error: ${error}`);
     return res.send('ERROR');
   }
+});
+
+router.get('/', (req, res) => {
+  if (!req.body.fileName) {
+    return res.status(402).send({ error: 'No image name given' });
+  }
+  return dbService.getStatus(req.body.fileName, (error, processingStatus) => {
+    if (processingStatus == null || error) {
+      return res.status(402).send({ error: `No image with name ${req.body.fileName} exists` });
+    }
+    if (error != null) {
+      return res.status(500).send({ error: `Generic error occured: ${error}` });
+    }
+
+    return res.sendFile(`${path.join(config.UPLOAD_DIR, req.body.fileName)}`);
+  });
 });
 
 router.get('/status', (req, res) => {
